753b65d1c418e401078c178a5de1e114bb2292d3
Sagar kumar <srk001929@gmail.com>
2026-09-11 11:17:23 +0530

    added update and getNoteById apis

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -44,3 +44,50 @@ export const getAllNotes = async (req, res) => {
     });
   }
 };
+
+export const getNoteById = async (req, res) => {
+  try {
+    const note = await Notes.findOne({ _id: req.params.id });
+    res.status(200).json({
+      message: "Note fetched successfully",
+      note: note,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error occured",
+      error: error.message,
+    });
+  }
+};
+
+export const updateNote = async (req, res) => {
+  const { title, content, semester, subject } = req.body;
+  try {
+    const note = await Notes.findOne({
+      _id: req.params.id,
+      createdBy: req.user.id,
+    });
+
+    if(!note){
+      res.status(400).json({
+        message: "Note not found"
+      })
+    }
+
+    note.title = title;
+    note.content = content;
+    note.semester = semester;
+    note.subject = subject;
+
+    await note.save();
+    res.status(200).json({
+      message: "Note updated successfully",
+      note : note,
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: "Error occured",
+      error: error.message,
+    });
+  }
+};
--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createNote, getAllNotes } from "../controllers/notesController.js";
+import { createNote, getAllNotes, getNoteById, updateNote } from "../controllers/notesController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/add-note", protect, createNote);
+router.get("/getNote/:id", getNoteById);
 router.get("/getNotes", getAllNotes);
+router.put("/updateNote/:id", protect, updateNote);
+
 
 export default router;
